24f6a75770c009aad942cc7c36cbc4401bf783ae
Ekaterina Vodolazhskaia <katik_v@inbox.ru>
2024-05-28 10:38:33 +0300

    added allure

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -1,71 +1,97 @@
 package tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.PracticeFormPage;
+import static io.qameta.allure.Allure.step;
 
 public class PracticeFormTests extends TestBase {
     PracticeFormPage practiceFormPage = new PracticeFormPage();
 
     @Test
+    @DisplayName("Регистрация с заполнением всех полей формы")
     void fillAllFieldsTest() {
 
-        practiceFormPage.openPage()
-                .removeBanners()
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setEmail(data.email)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
-                .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
-                .setSubjects(data.subject)
-                .setHobbies(data.hobby)
-                .uploadPicture(data.picture)
-                .setCurrentAddress(data.currentAddress)
-                .setState(data.state)
-                .setCity(data.city)
-                .submit()
+        step("Открыть форму регистрации", () -> {
+            practiceFormPage.openPage()
+                    .removeBanners();
+        });
 
-                .checkResult("Student Name", data.firstName + " " + data.lastName)
-                .checkResult("Student Email", data.email)
-                .checkResult("Gender", data.gender)
-                .checkResult("Mobile", data.userNumber)
-                .checkResult("Date of Birth", data.userBirthDay + " " + data.userBirthMonth + "," + data.userBirthYear)
-                .checkResult("Subjects", data.subject)
-                .checkResult("Hobbies", data.hobby)
-                .checkResult("Picture", data.picture)
-                .checkResult("Address", data.currentAddress)
-                .checkResult("State and City", data.state + " " + data.city);
+        step("Заполнить все поля формы и нажать submit", () -> {
+            practiceFormPage.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setEmail(data.email)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
+                    .setSubjects(data.subject)
+                    .setHobbies(data.hobby)
+                    .uploadPicture(data.picture)
+                    .setCurrentAddress(data.currentAddress)
+                    .setState(data.state)
+                    .setCity(data.city)
+                    .submit();
+        });
 
+        step("Проверить результат заполнения формы", () -> {
+            practiceFormPage.checkResult("Student Name", data.firstName + " " + data.lastName)
+                    .checkResult("Student Email", data.email)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Mobile", data.userNumber)
+                    .checkResult("Date of Birth", data.userBirthDay + " " + data.userBirthMonth + "," + data.userBirthYear)
+                    .checkResult("Subjects", data.subject)
+                    .checkResult("Hobbies", data.hobby)
+                    .checkResult("Picture", data.picture)
+                    .checkResult("Address", data.currentAddress)
+                    .checkResult("State and City", data.state + " " + data.city);
+        });
     }
 
     @Test
+    @DisplayName("Регистрация с заполнением только обязательных полей формы")
     void fillOnlyRequiredFieldsTest() {
-        practiceFormPage.openPage()
-                .removeBanners()
-                .setFirstName(data.firstName)
-                .setLastName(data.lastName)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
-                .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
-                .submit()
 
-                .checkResult("Student Name", data.firstName + " " + data.lastName)
-                .checkResult("Gender", data.gender)
-                .checkResult("Mobile", data.userNumber)
-                .checkResult("Date of Birth", data.userBirthDay + " " + data.userBirthMonth + "," + data.userBirthYear);
+        step("Открыть форму регистрации", () -> {
+            practiceFormPage.openPage()
+                    .removeBanners();
+        });
+
+        step("Заполнить обязательные поля формы и нажать submit", () -> {
+            practiceFormPage.setFirstName(data.firstName)
+                    .setLastName(data.lastName)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
+                    .submit();
+        });
+
+        step("Проверить результат заполнения формы", () -> {
+            practiceFormPage.checkResult("Student Name", data.firstName + " " + data.lastName)
+                    .checkResult("Gender", data.gender)
+                    .checkResult("Mobile", data.userNumber)
+                    .checkResult("Date of Birth", data.userBirthDay + " " + data.userBirthMonth + "," + data.userBirthYear);
+        });
     }
 
     @Test
+    @DisplayName("Регистрация с незаполненным обязательным полем email")
     void emptyFirstNameTest() {
-        practiceFormPage.openPage()
-                .removeBanners()
-                .setLastName(data.lastName)
-                .setGender(data.gender)
-                .setUserNumber(data.userNumber)
-                .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
-                .submit()
 
-                .checkEmptyFirstNameResult();
+        step("Открыть форму регистрации", () -> {
+            practiceFormPage.openPage()
+                    .removeBanners();
+        });
 
+        step("Заполнить обязательные поля формы, кроме email, и нажать submit", () -> {
+            practiceFormPage.setLastName(data.lastName)
+                    .setGender(data.gender)
+                    .setUserNumber(data.userNumber)
+                    .setDateOfBirth(data.userBirthDay, data.userBirthMonth, data.userBirthYear)
+                    .submit();
+        });
+
+        step("Проверить, что поле email выделено красной рамкой", () -> {
+            practiceFormPage.checkEmptyFirstNameResult();
+        });
     }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,7 +2,9 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import data.TestData;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +16,7 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
